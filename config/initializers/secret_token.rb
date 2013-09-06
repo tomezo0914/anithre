6249fd4d88ac8027791d8934897b1a872735ae1f
TOYOTOME Takeshi <tomezo0914@gmail.com>
2013-09-06 19:28:32 +0900

    create rails pj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anithre::Application.config.secret_key_base = 'bb7636707091811f8d79fb040582a9656fc88c3eb22692401762f67961f48103fbc1ae680374a1c30460f1a2cbd14309a4e59f35da3d46d56ac0582cf4763269'
---
+++ vendor/bundle/ruby/2.0.0/gems/therubyracer-0.12.0/ext/v8/siteconf20130906-5526-rsjbi2.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130906-5526-1a9mlx3"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
